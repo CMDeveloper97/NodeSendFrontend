7c32366004e6a34c0639d41d5f4a9b2736ab9a58
Carlos Mauricio <cmdeveloper97@gmail.com>
2021-12-28 20:47:09 -0700

    Change: NextConfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env:{
-    backendURL:"https://desolate-ridge-58771.herokuapp.com/",
+    backendURL:"https://node-send-15759.herokuapp.com/",
     frontendURL:"https://node-send-fontend-cmdeveloper97.vercel.app"
     //backendURL:"http://localhost:4000",
     //frontendURL:"http://localhost:3000"
